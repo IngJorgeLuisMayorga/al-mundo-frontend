1e05c24731eb04c88ffa7b9159bf43ea0bc7e135
TheSignLab Co <34943861+TheSignLab@users.noreply.github.com>
2018-07-26 00:53:58 -0500

    hj

--- src/app/lib/components/filter/filter.component.ts
+++ src/app/lib/components/filter/filter.component.ts
@@ -6,7 +6,40 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./filter.component.less"]
 })
 export class FilterComponent implements OnInit {
+  public filter = {
+    stars: [
+      {
+        value: 0,
+        range: []
+      },
+      {
+        value: 5,
+        range: Array.from(Array(5).keys())
+      },
+      {
+        value: 4,
+        range: Array.from(Array(4).keys())
+      },
+      {
+        value: 3,
+        range: Array.from(Array(3).keys())
+      },
+      {
+        value: 2,
+        range: Array.from(Array(2).keys())
+      },
+      {
+        value: 1,
+        range: Array.from(Array(1).keys())
+      }
+    ]
+  };
+
   constructor() {}
 
   ngOnInit() {}
+
+  public filterByStar(star) {
+    console.log(star.value);
+  }
 }
