1339f1df7a2b5b3a169849aedea6ef96f32956c7
TheSignLab Co <34943861+TheSignLab@users.noreply.github.com>
2018-07-26 01:07:29 -0500

    h

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 //Install express server
+const fs = require("fs");
 const express = require("express");
 const path = require("path");
 
@@ -9,6 +10,12 @@ app.use(express.static(__dirname + "/dist/almundo"));
 
 app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname + "/dist/almundo/index.html"));
+});
+
+app.get("/api/hotels", function(req, res) {
+  var usersFilePath = path.join(__dirname, "server/data.json");
+  var readable = fs.createReadStream(usersFilePath);
+  readable.pipe(res);
 });
 
 // Start the app by listening on the default Heroku port
